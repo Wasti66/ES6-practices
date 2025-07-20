f17a797d23ddd8262c9cbe79896457dd1ebfd665
Wazihatulla <08wasti@gmail.com>
2025-07-20 23:48:36 +0600

    spread operator added

---
+++ spread-operator.js
@@ -0,0 +1,7 @@
+//three dots means(...) spread operator
+
+let poorCountry = ['Bangladesh','Srilanka'];
+let richCountry = [...poorCountry,'Amarica','Canada','Italy'];
+console.log(poorCountry);
+console.log('\n');
+console.log(richCountry);
