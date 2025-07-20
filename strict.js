ad56336fa19be39936c4d37ba927843025e68723
Wazihatulla <08wasti@gmail.com>
2025-07-20 22:55:00 +0600

    I have added new file strict.js

---
+++ strict.js
@@ -0,0 +1,8 @@
+//(use strict method) is like a police
+"use strict"
+function myfunction(){
+    //let name = 'Wazihatulla Wasti';
+    name = 'Wazihatulla Wasti';
+    return name;
+}
+console.log(myfunction());
