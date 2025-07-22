d0fb155d386563f3181869050bbfecc4427717e1
Wazihatulla <08wasti@gmail.com>
2025-07-22 22:10:52 +0600

    object

---
+++ object.js
@@ -0,0 +1,16 @@
+let bioData = {
+    dataOne:{
+        name:'Wazihatulla Wasti',
+        email:'08wasti@gmail.com',
+        phone:'01968618766'
+    },
+    dataTwo:{
+        name:'Ruhani Islam Purnota',
+        email:'purnota54321@gmail.com',
+        phone:'01710520819'
+    }
+}
+console.log(
+    bioData['dataOne']['name'],
+    bioData['dataTwo']['name']
+);
