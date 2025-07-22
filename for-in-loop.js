701f80a2526379b1771160737ef42ac6b72eb257
Wazihatulla <08wasti@gmail.com>
2025-07-22 22:28:38 +0600

    for in loop

---
+++ for-in-loop.js
@@ -0,0 +1,10 @@
+let wasti = {
+    'Glasses':true,
+    'height':'5fit',
+    'Weight':'50kg',
+    'edu':'BSc in Computer science and engineering'
+}
+
+for(let property in wasti){
+    console.log(property+':'+wasti[property]);
+}
