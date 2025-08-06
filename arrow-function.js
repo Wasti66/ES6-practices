4430e7a59236a0eda0e21be2f410930de0aeaf15
Wazihatulla <08wasti@gmail.com>
2025-08-06 20:21:03 +0600

    arrow function

--- arrow-function.js
+++ arrow-function.js
@@ -2,3 +2,11 @@ let myFun =(x)=>{
     console.log(x);
 }
 myFun(20);
+
+const sum = (n1,n2)=>{
+    return 10 + 20;
+}
+console.log(sum());
+
+const sums = (n3,n4)=> n3 + n4;
+console.log(sums(40, 50));
